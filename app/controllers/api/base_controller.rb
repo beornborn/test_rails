3b064e6ef4161e137fdebd6192616764adf92a1f
Oleg Gorbunov <oleg.gorbunov-contractor@procore.com>
2025-03-10 10:48:58 +0200

    remove redundant

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -2,17 +2,5 @@
 
 module Api
   class BaseController < ApplicationController
-    private
-
-    def current_user
-      @current_user ||= begin
-        uuid = request.headers['X-User-UUID']
-        if uuid.present?
-          User.find_by(uuid: uuid) || User.create!(uuid: uuid)
-        else
-          User.create!(uuid: SecureRandom.uuid)
-        end
-      end
-    end
   end
 end
